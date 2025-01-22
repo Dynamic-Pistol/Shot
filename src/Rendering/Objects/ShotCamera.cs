9291469a971bb07bd70e72b4d2896bfdac938c02
Dynamic-Pistol <spidermalek666@gmail.com>
2025-01-22 20:29:29 +0200

    Update Camera

--- src/Rendering/Objects/ShotCamera.cs
+++ src/Rendering/Objects/ShotCamera.cs
@@ -5,12 +5,10 @@ namespace Shot.Rendering.Objects;
 
 public class ShotCamera
 {
-    public Matrix4 Projection => Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(Fov),
-        800.0f / 600.0f,
-        0.1f, 100f);
+    public Matrix4 Projection;
     private readonly Vector3 _cameraUp = Vector3.UnitY;
     public Vector3 Position { get; private set; } = new Vector3(0, 0, 6);
-    public Matrix4 View => Matrix4.LookAt(Position, Position + _cameraForward, _cameraUp);
+    public Matrix4 View;
     private Vector3 _cameraForward = -Vector3.UnitZ;
     public float Yaw = -90.0f;
     public float Pitch;
@@ -19,6 +17,14 @@ public class ShotCamera
     private bool _canRotate = true;
     private const float Speed = 6;
 
+    public void UpdateCamera()
+    { 
+        View = Matrix4.LookAt(Position, Position + _cameraForward, _cameraUp);
+        Projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(Fov),
+            800.0f / 600.0f,
+            0.1f, 100f);
+    } 
+        
     public void MoveCamera(Vector3 moveInput, float delta, float scrollDelta)
     {
         Position += Vector3.Normalize(Vector3.Cross(_cameraForward, _cameraUp)) * moveInput.X * Speed * delta;
